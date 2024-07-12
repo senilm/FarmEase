bc2260b8cfa210b53757882c3fe27bbb273746a8
senilm <senilmendapara2003@gmail.com>
2024-07-12 11:36:46 +0530

    Add: Date formats

--- frontend/src/lib/dateFormat.ts
+++ frontend/src/lib/dateFormat.ts
@@ -1,4 +1,6 @@
-export function formatDate(dateString: string): string {
+export function formatDate(dateString: string | undefined): string {
+  if(dateString){
+
     const date = new Date(dateString);
     const options: Intl.DateTimeFormatOptions = {
       day: 'numeric',
@@ -6,4 +8,22 @@ export function formatDate(dateString: string): string {
       year: 'numeric'
     };
     return new Intl.DateTimeFormat('en-US', options).format(date);
+  }else{
+    return ""
   }
+  }
+
+  export const reverseDate = (dateString: string) => {
+    return dateString.split('-').reverse().join('-');
+  }
+
+
+  export function formatDatePdf(dateString:string) {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 1-indexed
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+  
