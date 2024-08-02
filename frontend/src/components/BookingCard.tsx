2c32c379957dd44b650ee8eced6f8ff9ffcd09cc
senilm <senilmendapara2003@gmail.com>
2024-08-02 18:42:21 +0530

    Modified: Card UI

--- frontend/src/components/BookingCard.tsx
+++ frontend/src/components/BookingCard.tsx
@@ -11,7 +11,11 @@ const BookingCard: React.FC<BookingCardProps> = ({ data, refetch }) => {
   return (
     <div className="border px-4 py-3 rounded-lg mb-2 bg-white shadow-lg">
       <div className=" flex justify-between">
-        <p className=" font-bold text-xl text-green-500">₹{data.amount}</p>
+        <div>
+
+        <div className=" font-bold text-xl text-green-500">₹{data.amount}</div>
+        <div className=" font-semibold text-gray-500">{data.Farm.name}</div>
+        </div>
         <h3 className="font-bold">{data.User.name}</h3>
       </div>
       <div className=" flex justify-between items-center mt-2">
